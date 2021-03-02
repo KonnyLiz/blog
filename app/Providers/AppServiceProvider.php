818ace70aaa3a542437e9aad65232cad6e419a59
Konny Amaya <konnylizamaya@gmail.com>
2021-03-01 21:39:55 -0600

    creando la BD

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+// Usamos metodos de shema para darle valores por default a las 
+// migraciones de la BD
+use Illuminate\Support\Facades\Schema;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +27,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // aqui es donde les decimos que hacer a las migraciones
+
+        //la BD tendra por defecto 100 caracteres de largo
+        Schema::defaultStringLength(100);
     }
 }
