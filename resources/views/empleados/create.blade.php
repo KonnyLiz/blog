de56ad226ddfa408c8709b42315462d83cf8e739
Konny Amaya <konnylizamaya@gmail.com>
2021-03-02 21:48:54 -0600

    aplicando css form

--- resources/views/empleados/create.blade.php
+++ resources/views/empleados/create.blade.php
@@ -2,31 +2,35 @@
 
 @section('content')
 <div class="container">
-<h1>Crear empleado</h1>
-<!-- Mandando datos con instrucciones blade -->
-    <form method="post" action="{{ url('/empleados') }}" enctype="multipart/form-data">
-        
-        <!-- Llave que dejara entrar al metodo de laravel, crea un token -->
-        {{ csrf_field() }} 
+    <h1>Crear empleado</h1>
+    <!-- Mandando datos con instrucciones blade -->
+    <div class="form-group">
 
-        <label for="nombre">{{ 'Nombre' }}</label>
-        <input type="text" name="nombre" id="nombre">
-        <br>
+        <form method="post" action="{{ url('/empleados') }}" enctype="multipart/form-data">
 
-        <label for="apellido">{{ 'Apellido' }}</label>
-        <input type="text" name="apellido" id="apellido">
-        <br>
+            <!-- Llave que dejara entrar al metodo de laravel, crea un token -->
+            {{ csrf_field() }}
 
-        <label for="correo">{{ 'Correo' }}</label>
-        <input type="email" name="correo" id="correo">
-        <br>
+            <label for="nombre">{{ 'Nombre' }}</label>
+            <input type="text" name="nombre" id="nombre" class="form-control">
+            <br>
 
-        <label for="foto">{{ 'Foto' }}</label>
-        <input type="file" name="foto" id="foto">
-        <br>
+            <label for="apellido">{{ 'Apellido' }}</label>
+            <input type="text" name="apellido" id="apellido" class="form-control">
+            <br>
 
-        <input type="submit" value="Guardar">
-    </form>
+            <label for="correo">{{ 'Correo' }}</label>
+            <input type="email" name="correo" id="correo" class="form-control">
+            <br>
+
+            <label for="foto">{{ 'Foto' }}</label>
+            <input type="file" name="foto" id="foto">
+            <br>
+
+            <input type="submit" value="Guardar" class="btn btn-outline-primary">
+        </form>
+    </div>
+
 
     <a href="{{ url('empleados/') }}">Regresar</a>
 </div>
